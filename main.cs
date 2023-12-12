4a30714bf70bb11d43e86274e1164e08fe4cc601
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-12-12 12:37:01 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter message");
+    var post = Console.ReadLine();
+  {
+    if (post.Length > 140)
+      {
+        Console.WriteLine ("Rejected");
+      }
+    else 
+    {
+      Console.WriteLine ("Posted");
+      }
+    }
+    }
+  }
+  
